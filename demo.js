084168f9087905e4aa38d9766d10a8156b8676db
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-09-04 07:57:40 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,140 @@
+let z=20;
+console.log(typeof(z))
+let name1="sai";
+console.log(typeof(name1))
+let b=true;
+console.log(typeof(b))
+     
+x=10
+y=20
+console.log(x+y)
+
+num1=20
+num2=30
+console.log(num1*num2)
+
+a=40
+b=20
+console.log(a-b)
+
+let num3=2
+let num4=4
+console.log(num3/num4)  
+
+
+let age=2  
+
+if (age>18){
+  console.log(" eligible for vote")
+}else if(age<17){
+    console.log("not eligible for vote")
+}else{
+console.log("last chance for vote")  
+}
+
+//for loop
+// initialize, condition, increment
+
+for(let i=1;i<6;i++){
+    console.log("hi",i)
+}  
+
+//do while loop
+
+let num=0
+ do{
+    console.log(num);
+    num++; 
+
+}while(num<10);
+
+
+
+let istrue=true;
+let isfalse=false;
+
+if(istrue!=isfalse){
+    console.log("both condition is true")
+}
+else{
+    console.log("one is false")
+}
+
+
+{
+let a1=10
+let b2=20
+if(a1==b2)
+    console.log('both are equal')
+else
+    console.log('both are not same')}
+{
+ let a1=10
+let b2=20
+if(a1!=b2)
+    console.log('both are not equal')
+else
+    console.log('both are equal same')
+ 
+}
+
+//array
+
+let array=[20,30,40,50,'sai','nani']
+
+console.log(array)
+
+console.log(array.length)
+
+let myarray=[30,35,36,"bobby"]
+let myarray1=[4, 3,5,36,"sumi"]
+
+let newarray=myarray.concat(myarray1)
+
+console.log(newarray)
+
+
+//function
+
+function namebro(){
+   console.log("what is your name : sandeep")
+}
+namebro();
+
+
+
+let array1=[20,30,40,50,60]
+console.log(array1.length) //length
+
+let array2=[11,12,13,14,15]
+array2.pop() //pop
+console.log(array2)
+
+let array6=[23,24,25,26,27]
+console.log(array6.indexOf(25)) //index
+
+let array7=[240,41,42,43,230]
+array7.shift() //shift
+console.log(array7)
+
+let array8=[240,250,260,270,280]
+array8.unshift(400) //unshift
+console.log(array8)
+
+let array3=array1+array2
+console.log(array3)
+
+let array10=[380,340,235,456,123]
+console.log(array10.slice(0,2)) //slice  
+
+ 
+
+
+
+
+
+
+
+
+
+
