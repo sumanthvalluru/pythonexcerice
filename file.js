082371426525fe5257d4abf1118ca74a673398ee
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-10-28 20:53:30 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,30 @@
+let array1=[20,30,40,50,60]
+console.log(array1.length) //length
+
+let array2=[11,12,13,14,15]
+array2.pop() //pop
+console.log(array2)
+
+let array6=[23,24,25,26,27]
+console.log(array6.indexOf(25)) //index
+
+let array7=[240,41,42,43,230]
+array7.shift() //shift
+console.log(array7)
+
+let array8=[240,250,260,270,280]
+array8.unshift(400) //unshift
+console.log(array8)
+
+let array3=array1+array2
+console.log(array3)
+
+let array10=[380,340,235,456,123]
+console.log(array10.slice(0,2)) //slice  
+
+let arraysum=["sumi",10,20,30]
+console.log(arraysum.reverse()) //reverse
+
+let array40=[20,30,40,50]
+array40.push("raju")
+console.log(array40) //push
