98ba79e0780b299b91d225d287af6e793d3438c7
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-10-30 14:22:08 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,44 @@
+# Integer
+num_int = 42
+
+# Float
+num_float = 3.14
+
+# String
+str_text = "Hello, Python!"
+
+# List (mutable and ordered)
+my_list = [1, 2, 3, 4, 5]
+
+# Tuple (immutable and ordered)
+my_tuple = (10, 20, 30)
+
+# Set (mutable, unordered, and unique)
+my_set = {1, 2, 3, 4, 4, 5}
+
+# Dictionary (mutable, unordered, key-value pairs)
+my_dict = {"name": "Alice", "age": 25}
+
+# Boolean
+is_true = True
+is_false = False
+
+# None (a special type representing the absence of a value)
+my_none = None
+
+# Display the data types
+print("Integer:", num_int)
+print("Float:", num_float)
+print("String:", str_text)
+print("List:", my_list)
+print("Tuple:", my_tuple)
+print("Set:", my_set)
+print("Dictionary:", my_dict)
+print("Boolean (True):", is_true)
+print("Boolean (False):", is_false)
+print("None:", my_none)
+
+
+
+
+
