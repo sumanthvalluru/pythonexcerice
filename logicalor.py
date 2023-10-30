da4bd2dac3ca2d3ef1d05bfc4a73e7049d5571ab
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-10-30 14:26:03 +0530

    Add files via upload

---
+++ logicalor.py
@@ -0,0 +1,10 @@
+a=10
+b=-10
+if a>0 or b>0:
+    print("either of the num is greater than 0")
+else:
+    print("no num is greater than 0")
+    if b>0 or c>0:
+        print("either of the num is greater than 0")
+    else:
+        print("no num is greater than 0")
