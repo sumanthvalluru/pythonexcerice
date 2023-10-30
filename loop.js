467c61ff499eedb00b75f9ce9de6bc7d5e9d471e
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-10-30 09:06:00 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,45 @@
+//for loop
+// initialize, condition, increment
+
+for(let i=1;i<6;i++){
+    console.log("hi",i)
+}  
+
+//do while loop
+
+let num=0
+ do{
+    console.log(num);
+    num++; 
+
+}while(num<10);
+
+
+
+let istrue=true;
+let isfalse=false;
+
+if(istrue!=isfalse){
+    console.log("both condition is true")
+}
+else{
+    console.log("one is false")
+}
+
+
+{
+let a1=10
+let b2=20
+if(a1==b2)
+    console.log('both are equal')
+else
+    console.log('both are not same')}
+{
+ let a1=10
+let b2=20
+if(a1!=b2)
+    console.log('both are not equal')
+else
+    console.log('both are equal same')
+ 
+}
