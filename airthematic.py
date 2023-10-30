6ff7d67d30a967f5c7436a25210ba921f65e3ef1
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-10-30 14:21:25 +0530

    Add files via upload

---
+++ airthematic.py
@@ -0,0 +1,23 @@
+x = 15
+y = 7
+
+sum = x + y
+print("addition =", sum)
+
+subtraction = x - y
+print("subtraction =", subtraction)
+
+multiplication = x * y
+print("multiplication =", multiplication)
+
+division = x / y
+print("division =", division)
+
+modulus = x % y
+print("modulus =", modulus)
+
+exponent = x ** 2
+print("exponent =", exponent)
+
+floor_division = x // y
+print("division =", floor_division)
