82ab1625c14a80c42ee7d974f0c4b05d735e0a5a
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-10-30 14:27:49 +0530

    Add files via upload

---
+++ ifelif.py
@@ -0,0 +1,11 @@
+letter="A"
+if letter=="B":
+    print("letter is B")
+elif letter=="c":
+    print("letter is C")
+elif letter=="D":
+    print("letter is D")
+elif letter=="A":
+    print("letter is A")
+else:
+    print("letter isn't A,B,C or D")
