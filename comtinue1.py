180ac280f1bd1f8efb92c3a79b92d95b5ffe899f
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-10-30 14:21:49 +0530

    Add files via upload

---
+++ comtinue1.py
@@ -0,0 +1,10 @@
+# Python code to show example of continue statement  
+   
+# looping from 10 to 20  
+for iterator in range(10, 21):  
+   
+    # If iterator is equals to 15, loop will continue to the next iteration  
+    if iterator == 15:  
+        continue  
+    # otherwise printing the value of iterator  
+    print( iterator )  
