e2fb4278ed6d6311bd5e02168a974fb8dc288666
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-11-15 11:04:15 +0530

    Add files via upload

---
+++ index0.py
@@ -0,0 +1,45 @@
+
+
+# Integer
+integer_number = 10
+print("Integer:", integer_number)
+
+# Floating-point
+float_number = 3.14
+print("Float:", float_number)
+
+# Complex
+complex_number = 2 + 3j
+print("Complex:", complex_number)
+
+# Operations with numbers
+a = 5
+b = 2
+
+# Addition
+sum_result = a + b
+print("Sum:", sum_result)
+
+# Subtraction
+difference_result = a - b
+print("Difference:", difference_result)
+
+# Multiplication
+product_result = a * b
+print("Product:", product_result)
+
+# Division (result is always a float in Python 3)
+division_result = a / b
+print("Division:", division_result)
+
+# Floor Division (returns the floor of the division)
+floor_division_result = a // b
+print("Floor Division:", floor_division_result)
+
+# Modulus (returns the remainder of the division)
+modulus_result = a % b
+print("Modulus:", modulus_result)
+
+# Exponentiation
+exponential_result = a ** b
+print("Exponentiation:", exponential_result)
