3b5160ed3ea5ef0fd0ce61bdcc09ab5867d4da8f
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-11-20 12:53:07 +0530

    Add files via upload

---
+++ mult.js
@@ -0,0 +1,29 @@
+/* program to generate a multiplication table
+upto a range */
+
+// take number input from the user
+const number = parseInt(prompt('Enter an integer: '));
+
+// take range input from the user
+const range = parseInt(prompt('Enter a range: '));
+
+//creating a multiplication table
+for(let i = 1; i <= range; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+// program to generate a multiplication table
+
+// take input from the user
+const number1 = parseInt(prompt('Enter an integer: '));
+
+//creating a multiplication table
+for(let i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * number;
+
+    // display the result
+    console.log(`${number} * ${i} = ${result}`);
+}
