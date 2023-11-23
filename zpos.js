35b8f89fb30ab60a72ba31982a15301d5e980956
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-11-23 20:39:06 +0530

    Add files via upload

---
+++ zpos.js
@@ -0,0 +1,13 @@
+// to find the sum of positive numbers
+// if the user enters negative number, the loop terminates
+// negative number is not added to sum
+
+let sum = 0;
+let number = 0;
+
+do {
+    sum += number;
+    number = parseInt(prompt('Enter a number: '));
+} while(number >= 0)
+
+console.log(`The sum is ${sum}.`);
