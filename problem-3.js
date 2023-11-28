2f5d5813e7714de2370496815894cb1ee4f4bc88
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-11-28 20:56:00 +0530

    Add files via upload

---
+++ problem-3.js
@@ -0,0 +1,33 @@
+const localDate = new Date();
+const months = [
+   "January",
+   "February",
+   "March",
+   "April",
+   "May",
+   "June",
+   "July",
+   "August",
+   "September",
+   "October",
+   "November",
+   "December",
+];
+let currentMonth = months[localDate.getMonth()];
+console.log(currentMonth);
+const localDate = new Date();
+const currentDate = localDate.getDate();
+console.log(currentDate);
+const localDate = new Date();
+const days = [
+   "Sunday",
+   "Monday ",
+   "Tuesday",
+   "Wednesday",
+   "Thursday",
+   "Friday",
+   "Saturday",
+];
+
+const currentDay = days[localDate.getDay()];
+console.log(currentDay);
