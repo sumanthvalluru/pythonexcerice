4abb80211bdb3a4cfe6d95b006f2e65503b8031a
sumanthvalluru <140239664+sumanthvalluru@users.noreply.github.com>
2023-12-09 20:06:04 +0530

    Add files via upload

---
+++ error.js
@@ -0,0 +1,17 @@
+<script type = "text/javascript">
+
+      try {
+         // Code to run
+         [break;]
+      } 
+      
+      catch ( e ) {
+         // Code to run if an exception occurs
+         [break;]
+      }
+      
+      [ finally {
+         // Code that is always executed regardless of 
+         // an exception occurring
+      }]
+</script>
